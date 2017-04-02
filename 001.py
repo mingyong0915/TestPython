c706fa2c41dea9255887773d8307ebdb30b2b1ae
mingyong0915 <2228572119@qq.com>
2017-04-02 23:04:00 +0800

    add 001.py  test Exception add funtion exception

--- 001.py
+++ 001.py
@@ -12,8 +12,37 @@
 @time: 2017/4/2 22:31 
 """
 
+
 class MyException(Exception):
     pass
-
 myException = MyException("出错啦")
 print myException
+
+while True:
+    try:
+        x = input("Enter the first number:")
+        y = input("Enter the second number:")
+        value = x/y
+        print "x/y is ", value
+    except Exception, e:
+        print "Invalid input ,please try again."
+    else:
+        break
+
+# 函数处理异常
+
+
+def faulty():
+    raise Exception
+
+
+def ignore_exception():
+    faulty()
+
+
+def handle_exception():
+    try:
+        faulty()
+    except Exception:
+        print "handle exception"
+handle_exception()
