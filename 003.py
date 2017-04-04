845d0d0135a9a4e3bcde7ce107d3035b2823be01
mingyong0915 <2228572119@qq.com>
2017-04-04 09:50:29 +0800

    add 001.py  test Exception add funtion exception add 002.py add __setattr__  __getattr__ add 003.py

---
+++ 003.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python  
+# _*_ coding:utf-8 _*_
+
+""" 
+@version: v1.0 
+@author: MingYong 
+@license: Apache Licence  
+@contact: 13760182207@163.com 
+@site: http://blog.csdn.net/mingyong_blog 
+@software: PyCharm 
+@file: 003.py 
+@time: 2017/4/3 20:35 
+"""
+
+#python生成器
+#生成器：就是一个函数，一个产生(yield语句)值的函数，就是一种用普通的函数语法定义的迭代器
+
